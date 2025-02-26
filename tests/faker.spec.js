d84cfe7a90371131133dd63ee2959f7dac2893a5
Sergey Ganzeev <GanzeevSP@yandex.ru>
2025-02-26 23:14:20 +0700

    Подготовка файлов

--- tests/faker.spec.js
+++ tests/faker.spec.js
@@ -16,6 +16,54 @@ function getPassword() {
 };
 //const  getPassword = () =>  faker.internet.password();
 
+function newUser() {
+   // return faker.internet.password()
+};
+test('Sing Up', async ({ page }) => {
+    const USERNAME = getUsername();
+  await page.goto('https://realworld.qa.guru/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('Your Name').click();
+  await page.getByPlaceholder('Your Name').fill(USERNAME);
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(getEmail());
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(getPassword());
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  await expect(page.getByRole('navigation')).toContainText(USERNAME)
+});
+
+//Пользователь может опубликовать статью
+test('Sing Up', async ({ page }) => {
+    const USERNAME = getUsername();
+  await page.goto('https://realworld.qa.guru/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('Your Name').click();
+  await page.getByPlaceholder('Your Name').fill(USERNAME);
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(getEmail());
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(getPassword());
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  await expect(page.getByRole('navigation')).toContainText(USERNAME)
+});
+
+//Пользователь может оставить комментарий к статье
+test('Sing Up', async ({ page }) => {
+    const USERNAME = getUsername();
+  await page.goto('https://realworld.qa.guru/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('Your Name').click();
+  await page.getByPlaceholder('Your Name').fill(USERNAME);
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(getEmail());
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(getPassword());
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  await expect(page.getByRole('navigation')).toContainText(USERNAME)
+});
+
+//Пользователь может изменить пароль (не забудьте проверить, что можно залогиниться с новым паролем)
 test('Sing Up', async ({ page }) => {
     const USERNAME = getUsername();
   await page.goto('https://realworld.qa.guru/');
