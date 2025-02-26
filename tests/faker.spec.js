f904987259c1267af8a67112bfa5123198f7b642
Sergey Ganzeev <GanzeevSP@yandex.ru>
2025-02-26 23:12:58 +0700

    Подготовка данных и файлов

---
+++ tests/faker.spec.js
@@ -0,0 +1,31 @@
+
+// Подключение тестов
+import { test, expect } from '@playwright/test';
+// Подключение фейкера
+import { faker } from '@faker-js/faker';
+
+function getUsername() {
+    return faker.person.firstName();
+};
+
+function getEmail() {
+    return faker.internet.email();
+};
+function getPassword() {
+    return faker.internet.password()
+};
+//const  getPassword = () =>  faker.internet.password();
+
+test('Sing Up', async ({ page }) => {
+    const USERNAME = getUsername();
+  await page.goto('https://realworld.qa.guru/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('Your Name').click();
+  await page.getByPlaceholder('Your Name').fill(USERNAME);
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(getEmail());
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill(getPassword());
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  await expect(page.getByRole('navigation')).toContainText(USERNAME)
+});
